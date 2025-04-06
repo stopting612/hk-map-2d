7da72f17d73c6f31672fbe3f94eed3c40dc6e4bb
hoiting <stopting612@gmail.com>
2025-04-06 22:15:43 +0800

    finish custom icon

--- src/app/Components/map.js
+++ src/app/Components/map.js
@@ -1,22 +1,35 @@
 "use client";
-import React, { useEffect, useMemo, useCallback, useState} from "react";
-import { MapContainer, TileLayer, GeoJSON, useMap, Marker, Popup, } from "react-leaflet";
+import React, { useEffect, useMemo, useCallback, useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  GeoJSON,
+  useMap,
+  Marker,
+  Popup,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import locationList from "@/data/locationList.json";
+import { MaptilerLayer } from "@maptiler/leaflet-maptilersdk";
 
 // Fix for missing Leaflet marker icons (required for React-Leaflet)
 import L from "leaflet";
 
 //custom icon
-const customIcon = new L.Icon({
-  iconUrl: '/maps-and-flags.png', 
-  iconSize: [40, 41],      
-  iconAnchor: [12, 41],     
-  popupAnchor: [1, -34]    
+const locationMarker = new L.Icon({
+  iconUrl: "/maps-and-flags.png",
+  iconSize: [41, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
 });
-L.Marker.prototype.options.icon = customIcon;
+L.Marker.prototype.options.icon = locationMarker;
 
-
+const buildingIcon = new L.Icon({
+  iconUrl: "/a-tall-building-background.png",
+  iconSize: [100, 100],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
 
 function FlyToLocation({ location }) {
   const map = useMap();
@@ -24,7 +37,7 @@ function FlyToLocation({ location }) {
   useEffect(() => {
     if (location) {
       map.flyTo(location.coordinates, 15, {
-        duration: 1
+        duration: 1,
       });
     }
   }, [location, map]);
@@ -36,123 +49,126 @@ function CustomAttribution() {
   const map = useMap();
 
   useEffect(() => {
-    // Remove the footer , the footer will show the plugin  leaflet information 
+    // Remove the footer , the footer will show the plugin  leaflet information
     map.attributionControl.remove();
   }, [map]);
 
   return null;
 }
 
-function LocationFinder() {
-  const map = useMap();
-  const [position, setPosition] = useState(null);
+// function LocationFinder() {
+//   const map = useMap();
+//   const [position, setPosition] = useState(null);
 
-  useEffect(() => {
-    // Get user location when component mounts
-    map.locate({
-      setView: true,    // Center map on location
-      maxZoom: 16,      // Appropriate zoom level
-      timeout: 10000    // 10 second timeout
-    })
-    .on('locationfound', (e) => {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, 16); // Smooth zoom to location
-    })
-    .on('locationerror', (err) => {
-      console.error("Location access denied:", err.message);
-      // Default to Hong Kong coordinates if denied
-      const hkCoords = L.latLng(22.3193, 114.1694);
-      setPosition(hkCoords);
-      map.flyTo(hkCoords, 13);
-    });
+//   useEffect(() => {
+//     // Get user location when component mounts
+//     map.locate({
+//       setView: true,    // Center map on location
+//       maxZoom: 16,      // Appropriate zoom level
+//       timeout: 10000    // 10 second timeout
+//     })
+//     .on('locationfound', (e) => {
+//       setPosition(e.latlng);
+//       map.flyTo(e.latlng, 16); // Smooth zoom to location
+//     })
+//     .on('locationerror', (err) => {
+//       console.error("Location access denied:", err.message);
+//       // Default to Hong Kong coordinates if denied
+//       const hkCoords = L.latLng(22.3193, 114.1694);
+//       setPosition(hkCoords);
+//       map.flyTo(hkCoords, 13);
+//     });
 
-  }, [map]);
+//   }, [map]);
 
-  return position ? (
-    <Marker position={position} icon={customIcon}>
-      <Popup>You are here</Popup>
-    </Marker>
-  ) : null;
-}
-
-
+//   return position ? (
+//     <Marker position={position} icon={customIcon}>
+//       <Popup>You are here</Popup>
+//     </Marker>
+//   ) : null;
+// }
 
 function HKMap({ selectedLocation }) {
   console.log("Selected Location:", selectedLocation);
   const [rawGeoData, setRawGeoData] = useState(null);
-  const [activePopup, setActivePopup] = useState(null);
-
+  const [activePopup, setActivePopup] = useState(false);
 
   useEffect(() => {
-    fetch('../../../merged.json')
-      .then(response => response.json())
-      .then(data => setRawGeoData(data))
-      .catch(error => console.error('Error fetching GeoJSON:', error));
-  })
+    fetch("../../../merged.json")
+      .then((response) => response.json())
+      .then((data) => setRawGeoData(data))
+      .catch((error) => console.error("Error fetching GeoJSON:", error));
+  });
 
   // Style for GeoJSON features
-  const geoJsonStyle = useMemo(() => ({
-    color: '#3388ff',
-    weight: 2,
-    fillOpacity: 0.2
-  }), []);
+  const geoJsonStyle = useMemo(
+    () => ({
+      color: "#3388ff",
+      weight: 2,
+      fillOpacity: 0.2,
+    }),
+    []
+  );
 
-
-  
-  //This code is for tune the performance 
+  //This code is for tune the performance
   const optimizedGeoJSON = useMemo(() => {
     if (!rawGeoData) return null;
-    
+
     return {
       ...rawGeoData,
-      features: rawGeoData.features        
-        .filter(feature => feature.properties.CLASS === 'BDRY')
-        .map(feature => ({
+      features: rawGeoData.features
+        .filter((feature) => feature.properties.CLASS === "BDRY")
+        .map((feature) => ({
           ...feature,
-          geometry: simplifyGeometry(feature.geometry) 
-        }))
+          geometry: simplifyGeometry(feature.geometry),
+        })),
     };
-  }, []); 
-
-
+  }, []);
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
+      {/* Floating button container */}
+      <div className="absolute top-4 left-4 z-[1000] ml-8 space-x-2">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded shadow"
+          onClick={() => {
+            setActivePopup(!activePopup);
+          }}
+        >
+          Button 1
+        </button>
+      </div>
+
       <MapContainer
-        center={[22.3193, 114.1694]} // Hong Kong coordinates
-        zoom={12}
-        style={{ height: "100%", width: "100%" }}
-        bounds={[[22.3193, 114.1694], [22.3964, 114.1095]]} // Bounds for Hong Kong
-        maxBounds={[[22.15, 113.8], [22.6, 114.5]]} // Prevent panning outside of Hong Kong
+        center={[22.40048, 114.20955]} // Hong Kong
+        zoom={11}
+        scrollWheelZoom={true}
+        className="w-full h-screen"
       >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         <CustomAttribution />
 
-         {/* This will automatically locate user on mount */}
-      <LocationFinder />
-        
-        {selectedLocation // animation for the selected location
-         && <FlyToLocation location={selectedLocation} />}
+        {selectedLocation && ( // animation for the selected location
+          <FlyToLocation location={selectedLocation} />
+        )}
 
+        <Marker position={[22.3193, 114.1694]} icon={buildingIcon}>
+          <Popup>3d building</Popup>
+        </Marker>
 
-      {
-        locationList.map((location) => (
+        {locationList.map((location) => (
           <Marker key={location.id} position={location.coordinates}>
-        
             <Popup>
-            <div className="popup-content">
-              <h3>HI {location.name}</h3>
-    
-            </div>
-          </Popup>
-        
+              <div className="popup-content">
+                <h3>HI {location.name}</h3>
+              </div>
+            </Popup>
           </Marker>
-        ))
-      }
+        ))}
 
         {optimizedGeoJSON && (
           <GeoJSON
@@ -162,6 +178,31 @@ function HKMap({ selectedLocation }) {
           />
         )}
       </MapContainer>
+
+      {activePopup && (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[1000]">
+          <div className="bg-black p-6 rounded-lg shadow-xl max-w-md w-full border border-gray-200">
+            <h2 className="text-xl font-bold mb-3">Confirmation</h2>
+            <p className="mb-4">
+              Click anywhere on the map to close this popup.
+            </p>
+            <div className="flex space-x-3">
+              {/* <button
+                className="flex-1 bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded"
+                onClick={() => alert('Action triggered!')}
+              >
+                Action
+              </button> */}
+              <button
+                className="flex-1 bg-gray-200 hover:bg-gray-300 py-2 px-4 rounded"
+                onClick={() => setActivePopup(false)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
